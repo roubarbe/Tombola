e9648bf481ac50d84b63de901266635873c18ae9
Xavier <provost.pascal@gmail.com>
2019-10-12 11:04:35 -0400

    Interface work

--- scripts/main.js
+++ scripts/main.js
@@ -1,21 +1,29 @@
 //main.js
 
-let drawingList = [
-    "Alexandre",
-    "Alexandre",
-    "Simon",
-    "Léandre",
-    "Red",
-    "Green",
-    "Blue",
-    "Pistache",
-    "Alexandre",
-    "Léandre",
-    "Roxanne",
-    "Jean-Francois",
-    "Xavier",
-    "Alexandre"
-];
+
+// let drawingList = [
+//     "Alexandre",
+//     "Alexandre",
+//     "Simon",
+//     "Léandre",
+//     "Red",
+//     "Green",
+//     "Blue",
+//     "Pistache",
+//     "Alexandre",
+//     "Léandre",
+//     "Roxanne",
+//     "Jean-Francois",
+//     "Xavier",
+//     "Alexandre"
+// ];
+
+let newDraw;
+
+/*{
+["Alexandre","Alexandre","Simon","Léandre","Red","Green","Blue","Pistache","Alexandre","Léandre","Roxanne","Jean-Francois","Xavier","Alexandre"]
+}*/
+
 
 let title = new Vue({
     el: "#title",
@@ -25,13 +33,120 @@ let title = new Vue({
 });
 
 let formDataTypeSelect = new Vue({
-    el: "#typeData",
+    el: "#configureDrawing",
     data:{
         selected:"0",
         options:[
             {text: "Excel file", value: "0"},
             {text: "Text", value: "1"},
-            {text: "JSON file", value: "2"}
-        ]
+            {text: "JSON", value: "2"}
+        ],
+        selectLabel: "Choose data type:",
+        nbParticipants:{
+            placeholder: "",
+            label: "Number of participants (all if none entered):"
+        },
+        participantsData:{
+            placeholder: "Enter / Paste textual data here"
+        },
+        submitText: "Go!"
     }
 });
+
+
+
+let mainDrawing = new Vue({
+    el: "#mainDrawing",
+    data:{
+        winners: "Winners",
+        participants: "Participants",
+        submit: "Draw!",
+        // winnersList:[
+        //     "Test",
+        //     "Autre test",
+        //     "Encore un autre test"
+        // ]
+        winnersList: [],
+        participantsList: []
+    },
+    methods:{
+        update: function(){
+            //this.winnersList = newDraw.drawed;
+            //this.participantsList = newDraw.drawingList;
+            let newWinnersArray = newDraw.drawed;
+            this.winnersList = newWinnersArray;
+
+            let newParticipantsArray = newDraw.drawingList;
+            this.participantsList = newParticipantsArray;
+        },
+        draw: function(){
+            newDraw.draw();
+            this.update();
+        }
+    }
+});
+
+
+
+/**
+ * Starts the drawing process
+ */
+function startDrawing(){
+
+    switch(document.querySelector("#typeData").value){
+        case "0":
+            console.log("unimplemented");
+            break;
+        case "1":
+            console.log("unimplemented");
+            break;
+        case "2":
+            try{
+                let jsonData = JSON.parse(document.querySelector("#dataText").value);
+                let nbParticipantsValue = document.querySelector("#nbParticipants").value;
+                
+                //Security because don't trust user inputs
+                if(nbParticipantsValue == undefined){
+                    nbParticipantsValue = "";
+                }
+                
+                document.querySelector("#configureDrawing").classList.add("animated","fadeOutLeft","faster");
+                document.querySelector("#configureDrawing").addEventListener('animationend', ev => {
+                    document.querySelector("#configureDrawing").style.display = "none";
+                    document.querySelector("#mainDrawing").style.display = "block";
+                    document.querySelector("#mainDrawing").classList.add("animated","fadeInDown");
+                    document.querySelector("#mainDrawing").addEventListener('animationend', ev => {
+                        newDraw = new TombolaJS(jsonData, nbParticipantsValue);
+                        //mainDrawing.winnersList = newDraw.drawed;
+                        //mainDrawing.participantsList = newDraw.drawingList;
+                        mainDrawing.update();
+                        //document.querySelector("#submitButtonDiv").setAttribute("onclick","newDraw.draw()");
+                    });
+                    
+                });
+            }
+            catch(e){
+                console.log(e);
+            }
+            break;
+        default:
+            console.log("How were you able to submit this without a value?");
+            break;
+    }
+
+    // if(document.querySelector("#typeData").value == 0){
+
+    // }
+    // if(document.querySelector("#typeData").value == 2){
+    //     try{
+    //         let jsonData = JSON.parse(document.querySelector("#dataText").value);
+    //         document.querySelector("#configureDrawing").classList.add("animated","fadeOutLeft","faster");
+    //         document.querySelector("#configureDrawing").addEventListener('animationend', ev => {
+
+    //         });
+    //     }
+    //     catch(e){
+    //         console.log(e);
+    //     }
+    // }
+}
--- scripts/tombola.js
+++ scripts/tombola.js
@@ -1,12 +1,13 @@
 //tombola.js
 
 class TombolaJS{
-    constructor(drawingList, nbOfWinners){
-        //Removes duplicates by converting to a set and back to an array;
-        this.drawingList = Array.from(new Set(drawingList));
+    constructor(drawingListPassed, nbOfWinners){
+        //Fetch list of all participants
+        // this.drawingList = drawingList;
+        this.drawingList = drawingListPassed;
         
         //If no allowed numbers of winners are specified, it's set to the full length of the participants list
-        if(nbOfWinners === undefined){
+        if(nbOfWinners === undefined || nbOfWinners === "0" || nbOfWinners === ""){
             this.nbOfWinners = this.drawingList.length;
         }
         else{
@@ -15,6 +16,14 @@ class TombolaJS{
         
         //Set list of drawed participants as an array;
         this.drawed = [];
+    }
+
+    static get drawed() {
+        return drawed;
+    }
+
+    static get drawingList() {
+        return drawingList;
     }
 
     draw(){
@@ -27,13 +36,15 @@ class TombolaJS{
                 
                 //Choose a random number between 0 and length of number of participants remaining
                 let randomNumber = Math.floor(Math.random() * this.drawingList.length);
-
-                //console.log("Length of drawingList: "+this.drawingList.length+", Number rolled: "+randomNumber);
                 
-                //We're storing the winner in a variable, as a return to removing it from the list of participants
-                let winner = this.drawingList.splice(randomNumber, 1).toString();
+                //We're storing the winner in a variable
+                let winner = this.drawingList[randomNumber].toString();
 
-                //console.log("Winner: "+winner);
+                //And now we remove this winner from the participants list
+                let drawingListCopy = this.drawingList;
+                this.drawingList = this.drawingList.filter(function(value, index, drawingListCopy){
+                    return value != winner; 
+                });
                 
                 //And we're inserting it into the list of drawed participants
                 this.drawed.push(winner);
